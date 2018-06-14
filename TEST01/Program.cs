d4242c5e0ccea41b3700870a04a4e9993b017204
cplkimth <cplkimth@naver.com>
2018-06-14 22:49:48 +0900

    .

--- TEST01/Program.cs
+++ TEST01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Runtime.Remoting.Channels;
 
 namespace TEST01
 {
@@ -14,6 +11,7 @@ namespace TEST01
             Map.Instance.Initialize(3,3);
             Map.Instance.BuildBlocksForTest();
             List<Point> points = Map.Instance.GetMovableArea(0x11, 1);
+            points.ForEach(x => Console.WriteLine(x));
             Console.WriteLine(points.Count);
         }
     }
--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -1,72 +1,72 @@
-﻿using System;
+﻿#region
 using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TEST01;
+#endregion
 
 namespace TestProject1
 {
-    [TestFixture]
+    [TestClass]
     public class Tests
     {
-        [SetUp]
+        [TestInitialize]
         public void Init()
         {
-            Map.Instance.Initialize(3,3);
+            Map.Instance.Initialize(3, 3);
             Map.Instance.BuildBlocksForTest();
         }
-        
-        [Test]
+
+        [TestMethod]
         public void 원점에서_1칸_이동()
         {
             // R * * 
             // O * * 
             // S X * 
-            
+
             List<Point> points = Map.Instance.GetMovableArea(0, 1);
-            
+
             Assert.AreEqual(1, points.Count);
             Assert.IsTrue(points.Contains(0x01));
         }
-        
-        [Test]
+
+        [TestMethod]
         public void 가운데에서_1칸_이동()
         {
             // R O * 
             // O S O 
             // * X * 
-            
+
             List<Point> points = Map.Instance.GetMovableArea(0x11, 1);
-            
+
             Assert.AreEqual(3, points.Count);
             Assert.IsTrue(points.Contains(0x01));
             Assert.IsTrue(points.Contains(0x12));
             Assert.IsTrue(points.Contains(0x21));
         }
-        
-        [Test]
+
+        [TestMethod]
         public void 원점에서_2칸_이동()
         {
             // R * * 
             // * O * 
             // S X * 
-            
+
             List<Point> points = Map.Instance.GetMovableArea(0, 2);
-            
+
             Assert.AreEqual(2, points.Count);
             Assert.IsTrue(points.Contains(0x02));
             Assert.IsTrue(points.Contains(0x11));
         }
-        
-        [Test]
+
+        [TestMethod]
         public void 원점에서_3칸_이동()
         {
             // R O * 
             // * * O 
             // S X * 
-            
+
             List<Point> points = Map.Instance.GetMovableArea(0, 2);
-            
+
             Assert.AreEqual(3, points.Count);
             Assert.IsTrue(points.Contains(0x12));
             Assert.IsTrue(points.Contains(0x21));
